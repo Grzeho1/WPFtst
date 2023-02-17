02ef5fe8a4ca3b8b7faca822db8f7174edf686d5
tomas rehak <rehyy88@gmail.com>
2023-02-18 00:28:51 +0100

    updt

--- AppMain.xaml.cs
+++ AppMain.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using WPFtest2.Pages;
